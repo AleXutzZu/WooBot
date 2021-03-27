2fea477408b5040430a23b12992ce1b2521e2aea
AleXutzZu <davidalexrobertyt@gmail.com>
2021-03-27 17:03:07 +0200

    Redesigns

--- src/main/java/me/alexutzzu/woobot/utils/constants/Links.java
+++ src/main/java/me/alexutzzu/woobot/utils/constants/Links.java
@@ -3,6 +3,8 @@ package me.alexutzzu.woobot.utils.constants;
 public enum Links {
     SUPPORT_SERVER("https://discord.gg/DWFCgXw9"),
     CONTROL_PANEL("Coming soon!");
+
+
     private final String link;
     Links(String link) {
         this.link=link;
